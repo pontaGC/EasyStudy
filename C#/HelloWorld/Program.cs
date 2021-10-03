a4b56353f86dc7ba78265148a2b770b7cadcb35a
pontaGC <makizawawa@gmail.com>
2021-10-03 19:14:35 +0900

    Creates hello word!

---
+++ C#/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
