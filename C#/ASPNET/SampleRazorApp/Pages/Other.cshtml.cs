d81f61e9e071dcad0b379a9ced845535e99bd249
pontaGC <makizawawa@gmail.com>
2022-05-16 12:43:41 +0900

    Check point (3-4 section, Html methods in ASP.NET Core3) in SampleRazorApp

--- C#/ASPNET/SampleRazorApp/Pages/Other.cshtml.cs
+++ C#/ASPNET/SampleRazorApp/Pages/Other.cshtml.cs
@@ -7,18 +7,46 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SampleRazorApp.Pages
 {
+    public enum Gender
+    {
+        Male,
+        Female,
+    }
+
+    public enum Platform
+    {
+        Windows,
+        MacOS,
+        Linux,
+        ChromeOS,
+        Android,
+        iOS,
+    }
+
     public class OtherModel : PageModel
     {
+        #region Properties
+
         public string Message { get; set; }
+
+        public bool Check { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public Platform Platform { get; set; }
+
+        public Platform[] Platforms { get; set; }
+
+        #endregion
 
         public void OnGet()
         {
-
+            this.Message = "check & select it";
         }
 
-        public void OnPost()
+        public void OnPost(bool check, string gender, Platform platform, Platform[] platforms)
         {
-            this.Message = $"“ü—Í’l: {Request.Form["msg"]}";
+            this.Message = $"Result: {check}, {gender}, {platform} {platforms.Length}";
         }
     }
 }
