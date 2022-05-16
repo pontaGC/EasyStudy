d641e3987d4cb99401498ad2e215b297e1fe6dd2
pontaGC <makizawawa@gmail.com>
2022-05-16 12:05:29 +0900

    Check point (3.2 section in ASP.Core 3) in  SampleRazorApp.

--- C#/ASPNET/SampleRazorApp/Pages/Index.cshtml.cs
+++ C#/ASPNET/SampleRazorApp/Pages/Index.cshtml.cs
@@ -5,21 +5,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleRazorApp.Pages
 {
     public class IndexModel : PageModel
     {
+        #region Fields
+
         private readonly ILogger<IndexModel> _logger;
+
+        #endregion
+
+        #region Constructors
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+        #endregion
+
+        #region Properties
+
+        // BindProperty: クエリ文字列の値をモデルプロパティにバインディング
+        // SupportsGet: GETメソッドによるアクセスを許可する(= "Id"という名前のクエリパラメータ値がそのままプロパティに格納される)
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; }
+
+        public string Message { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Mail { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Tel { get; set; }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// このページにGETアクセスしたときに実行したい処理をこのメソッドに記述する
+        /// </summary>
         public void OnGet()
         {
+            this.Message = "入力してください";
+        }
+
+        public void OnPost(string name, string password, string mail, string tel)
+        {
+            this.Message = $"Name: {name} Password:({password.Length} chars) Mail: {mail} TEL: <{tel}>";
 
         }
+
+        #endregion
     }
 }
---
+++ C#/ASPNET/SampleRazorApp/Pages/Other.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SampleRazorApp.Pages
+{
+    public class OtherModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            this.Message = $"“ü—Í’l: {Request.Form["msg"]}";
+        }
+    }
+}
