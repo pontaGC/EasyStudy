f3858205bd88b9aefde324f66161939b5aa3b8a5
pontaGC <makizawawa@gmail.com>
2022-05-28 20:35:28 +0900

    Adds some atributes in Persion model

--- C#/ASPNET/SampleMVC/SampleMVCApp/Models/Person.cs
+++ C#/ASPNET/SampleMVC/SampleMVCApp/Models/Person.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace SampleMVCApp.Models
 {
@@ -11,10 +15,16 @@ namespace SampleMVCApp.Models
 
         public int PersonId { get; set; }
 
+        [Required]
+        [Index(IsUnique = true)]
+        [StringLength(256)]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
 
+        [DataType(DataType.EmailAddress)]
         public string Mail { get; set; }
 
+        [DataType(DataType.Text)]
         public int Age { get; set; }
     }
 }
